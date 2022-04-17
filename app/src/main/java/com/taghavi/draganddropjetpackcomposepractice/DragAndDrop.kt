438e4f259c61d33b22f80580e7d97f05dd51a839
EhsanTgv <EhsanTgv@gmail.com>
2022-04-17 18:56:25 +0300

    Implement DragTarget

---
+++ app/src/main/java/com/taghavi/draganddropjetpackcomposepractice/DragAndDrop.kt
@@ -0,0 +1,80 @@
+package com.taghavi.draganddropjetpackcomposepractice
+
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+
+internal val LocalDragTargetInfo = compositionLocalOf { DragTargetInfo() }
+
+@Composable
+fun <T> DragTarget(
+    modifier: Modifier,
+    dataToDrop: T,
+    content: @Composable (() -> Unit)
+) {
+    var currentPosition by remember { mutableStateOf(Offset.Zero) }
+    var currentState = LocalDragTargetInfo.current
+
+    Box(modifier = modifier
+        .onGloballyPositioned {
+            currentPosition = it.localToWindow(Offset.Zero)
+        }
+        .pointerInput(Unit) {
+            detectDragGesturesAfterLongPress(onDragStart = {
+                currentState.dataToDrop = dataToDrop
+                currentState.isDragging = true
+                currentState.dragPosition = currentPosition + it
+                currentState.draggableComposable = content
+            }, onDrag = { change, dragAmount ->
+                change.consumeAllChanges()
+                currentState.dragOffset += Offset(dragAmount.x, dragAmount.y)
+            }, onDragEnd = {
+                currentState.isDragging = false
+                currentState.dragOffset = Offset.Zero
+            }, onDragCancel = {
+                currentState.dragOffset = Offset.Zero
+                currentState.isDragging = false
+            })
+        }) {
+        content()
+    }
+}
+
+@Composable
+fun <T> DropTarget(
+    modifier: Modifier,
+    content: @Composable() (BoxScope.(isInBound: Boolean, data: T?) -> Unit)
+) {
+
+    val dragInfo = LocalDragTargetInfo.current
+    val dragPosition = dragInfo.dragPosition
+    val dragOffset = dragInfo.dragOffset
+    var isCurrentDropTarget by remember {
+        mutableStateOf(false)
+    }
+
+    Box(modifier = modifier.onGloballyPositioned {
+        it.boundsInWindow().let { rect ->
+            isCurrentDropTarget = rect.contains(dragPosition + dragOffset)
+        }
+    }) {
+        val data =
+            if (isCurrentDropTarget && !dragInfo.isDragging) dragInfo.dataToDrop as T? else null
+        content(isCurrentDropTarget, data)
+    }
+}
+
+internal class DragTargetInfo {
+    var isDragging: Boolean by mutableStateOf(false)
+    var dragPosition by mutableStateOf(Offset.Zero)
+    var dragOffset by mutableStateOf(Offset.Zero)
+    var draggableComposable by mutableStateOf<(@Composable () -> Unit)?>(null)
+    var dataToDrop by mutableStateOf<Any?>(null)
+}
